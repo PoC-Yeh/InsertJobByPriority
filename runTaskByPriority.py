c32fde33a9e05aa8328a1e07b867a169f6480fbd
PoC-Yeh <allisonxp7@gmail.com>
2023-06-02 21:45:11 -0400

    Update runTaskByPriority.py

--- runTaskByPriority.py
+++ runTaskByPriority.py
@@ -93,31 +93,3 @@ def execute(priorityLagerToSmall=True):
 if __name__ == '__main__':
     execute()
 
-"""
-result would be like:
-
-Tasks generated:
-[{'command': 'mnDO_0', 'priority': 2},
- {'command': 'KsEn_1', 'priority': 8},
- {'command': 'TpKE_2', 'priority': 10},
- {'command': 'vyLL_3', 'priority': 3},
- {'command': 'Gybo_4', 'priority': 9},
- {'command': 'cDIN_5', 'priority': 6},
- {'command': 'IObN_6', 'priority': 3},
- {'command': 'HglU_7', 'priority': 5},
- {'command': 'ERRH_8', 'priority': 7},
- {'command': 'Yzvb_9', 'priority': 9}]
-
-[Run tasks] --------------------
-run command: TpKE_2, task priority: 10
-run command: Gybo_4, task priority: 9
-run command: Yzvb_9, task priority: 9
-run command: KsEn_1, task priority: 8
-run command: ERRH_8, task priority: 7
-run command: cDIN_5, task priority: 6
-run command: HglU_7, task priority: 5
-run command: vyLL_3, task priority: 3
-run command: IObN_6, task priority: 3
-run command: mnDO_0, task priority: 2
-
-"""
